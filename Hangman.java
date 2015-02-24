46d7aed68e56c1435272ca313290c42edf167dbc
S <s>
2015-02-24 17:28:49 +0200

    {"type":"TI", "runs":"3"}

--- Hangman.java
+++ Hangman.java
@@ -51,6 +51,10 @@ public class Hangman extends ConsoleProgram {
 	    		} else {
 	    			println("Wrong letter");
 	    			guesses --;
+	    			if (guesses == 0) {
+	    		       	println("GAME OVER!");
+	    		       	break;
+	    			}
 	    		}
 	    	}
 	    	// Victory for the user
@@ -61,7 +65,7 @@ public class Hangman extends ConsoleProgram {
 	    	}
         }
        	
-       	println("GAME OVER!");
+
     	//TODO: Implement the basic control structure and manage the details 
     	// (ask the user to guess a letter, keep track of the number of guesses remaining, print out the various messages, detect the end of the game, and so forth).
 		
