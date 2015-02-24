447bc7ebe938129e4cda01fb1859c05627e2b419
S <s>
2015-02-24 17:30:26 +0200

    {"type":"TI", "runs":"3"}

--- Hangman.java
+++ Hangman.java
@@ -47,7 +47,7 @@ public class Hangman extends ConsoleProgram {
 	    		indexOfTheGuessedLetter = randomWord.indexOf(userInput);
 	    		if ( indexOfTheGuessedLetter != -1) {
 	    			guessedWord = guessedWord.substring(0, indexOfTheGuessedLetter) + userInput + guessedWord.substring(indexOfTheGuessedLetter + 1);
-	    			println(guessedWord);
+	    			println("Your guessed word is: " + guessedWord);
 	    		} else {
 	    			println("Wrong letter");
 	    			guesses --;
@@ -64,25 +64,6 @@ public class Hangman extends ConsoleProgram {
 	    		break;
 	    	}
         }
-       	
-
-    	//TODO: Implement the basic control structure and manage the details 
-    	// (ask the user to guess a letter, keep track of the number of guesses remaining, print out the various messages, detect the end of the game, and so forth).
-		
-	/*	
-		
-		
-		The sample runs in Figure 1 should be sufficient to illustrate the basic operation of the game, but the following points may help to clarify a few issues:
-		• At the beginning of your run method, you need to create a new HangmanLexicon and store it in an instance variable. 
-		If you extend the program to allow the user to play multiple games, 
-		the creation of the HangmanLexicon should be performed outside the loop that plays the game repeatedly so that this operation is performed once rather than for every game.
-		• You should accept the user’s guesses in either lower or upper case, even though all letters in the secret words are written in upper case.
-		• If the user guesses something other than a single letter, your program should tell the user that the guess is illegal and accept a new guess.
-		• If the user guesses a correct letter more than once, your program should simply do nothing. 
-		Guessing an incorrect letter a second time should be counted as another wrong guess. 
-		(In each case, these interpretations are the easiest way to handle the situation, and your program will probably do the right thing even if you don’t think about these cases in detail.)
-		Remember to finish Part I before moving on to Part II. Part II is arguably more fun, but it is essential to develop large programs in manageable stages.
-		*/
 	}
     
     // Gets a random word from the HangmanLexicon class
