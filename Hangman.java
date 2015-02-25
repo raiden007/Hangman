953702e19dac43456c5ffc984ed08256b4a5d0e4
S <s>
2015-02-25 12:14:53 +0200

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -19,37 +19,18 @@ public class Hangman extends ConsoleProgram {
     	String randomWord = getRandomWord();
     	println(randomWord);
     	// Display the guessed word
-    	String guessedWord = "";
-   		for (int i = 0; i < randomWord.length(); i++) {
-   			guessedWord += "-";
-   		}
-    	println("Your guessed word is: " + guessedWord);
-		String newWord = "";
-    	// Split the word into characters
-    	for (int i = 0; i < guessedWord.length(); i++) {
-    		char letter = guessedWord.charAt(i);
-    		newWord = newWord + letter;
-    		println(newWord);
-    	}
-		println();
+    	populateGuessedWord();
+    	
     	guesses = 8;
        	while (guesses > 0) {   
         	println("You have " + guesses + " guesses left!");
        		// User input
-        	String userInput = readLine("Enter a letter: ");
-        	userInput = userInput.toUpperCase();	
-        	char guessedLetter = userInput.charAt(0);
-	    	boolean isLetter = Character.isLetter(guessedLetter);
-	    	
+        	getUserInput();
 	    	// Check if there are more letter or they are not letters
-	    	if (userInput.length() > 1) {
-	    		println("Only one letter");
-	    	} else if (isLetter == false) {
-	    		println ("Only letters are accepted");
+
 	    		
 	    	// Add characters to the guessed word
 	    	//TODO: If you have the same letter twice it will not show the second one
-	    	} else {
 	    		int indexOfTheGuessedLetter = 0;
 	    		indexOfTheGuessedLetter = randomWord.indexOf(userInput);
 	    		if ( indexOfTheGuessedLetter != -1) {
@@ -68,12 +49,32 @@ public class Hangman extends ConsoleProgram {
 	    	char victoryCondition = '-'; 
 	    	if (guessedWord.indexOf(victoryCondition) == -1) {
 	    		println("YOU WON!");
-	    		break;
 	    	}
         }
-	}
+	
      
-    // Gets a random word from the HangmanLexicon class
+    private void getUserInput() {
+    	String userInput = readLine("Enter a letter: ");
+    	userInput = userInput.toUpperCase();
+    	boolean isLetter = Character.isLetter(userInput.charAt(0));
+    	// Check it's only one letter
+    	if (userInput.length() > 1) {
+    		println("Only one letter");
+    	// Check it's a letter
+    	} else if (isLetter == false) {
+    		println ("Only letters are accepted");
+    	}
+	}
+
+	private void populateGuessedWord() {
+   		for (int i = 0; i < randomWord.length(); i++) {
+   			guessedWord += "-";
+   		}
+    	println("Your guessed word is: " + guessedWord);
+		
+	}
+
+	// Gets a random word from the HangmanLexicon class
     private String getRandomWord() {
     	int randomindex = rgen.nextInt(0,lexicon.getWordCount()-1);
     	String randomWord = lexicon.getWord(randomindex);
@@ -86,4 +87,7 @@ public class Hangman extends ConsoleProgram {
 	private RandomGenerator rgen = new RandomGenerator();
 	private HangmanLexicon lexicon = new HangmanLexicon();
 	private int guesses;
+	String randomWord;
+	String guessedWord;
+	String userInput;
 }
