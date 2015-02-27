156da68fb56ab7e2039238b23f31e3f2f39af742
S <s>
2015-02-27 18:26:51 +0200

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -76,7 +76,8 @@ private void drawRightLeg() {
 private void drawLeftLeg() {
 	GLine leftHip = new GLine (245,95+BODY_LENGTH, 245 - HIP_WIDTH, 95+BODY_LENGTH );
 	add (leftHip);
-	
+	GLine leftLeg = new GLine (245 - HIP_WIDTH, 95+BODY_LENGTH, 245 - HIP_WIDTH, 95+BODY_LENGTH+LEG_LENGTH);
+	add (leftLeg);
 }
 
 private void drawRightArm() {
