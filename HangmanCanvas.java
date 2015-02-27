630c0368dbe3f4f136a4238b0c7dced6062d8ae3
S <s>
2015-02-27 18:21:29 +0200

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -80,8 +80,8 @@ private void drawLeftLeg() {
 private void drawRightArm() {
 	GLine rightArm = new GLine (245,95 + ARM_OFFSET_FROM_HEAD,245+UPPER_ARM_LENGTH,95 + ARM_OFFSET_FROM_HEAD);
 	add (rightArm);
-	GLine leftHand = new GLine (245-UPPER_ARM_LENGTH,95 + ARM_OFFSET_FROM_HEAD, 245-UPPER_ARM_LENGTH, 95 + ARM_OFFSET_FROM_HEAD+LOWER_ARM_LENGTH);
-	add (leftHand);
+	GLine rightHand = new GLine (245-UPPER_ARM_LENGTH,95 + ARM_OFFSET_FROM_HEAD, 245-UPPER_ARM_LENGTH, 95 + ARM_OFFSET_FROM_HEAD+LOWER_ARM_LENGTH);
+	add (rightHand);
 	
 }
 
