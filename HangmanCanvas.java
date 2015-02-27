7ee378226d801ff61f768ce7c963f8b6fa57061b
S <s>
2015-02-27 18:12:53 +0200

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -93,7 +93,7 @@ private void drawBody() {
 }
 
 private void drawHead() {
-	GOval head = new GOval (240,100,HEAD_RADIUS, HEAD_RADIUS);
+	GOval head = new GOval (200,50,HEAD_RADIUS, HEAD_RADIUS);
 	add (head);
 	
 }
