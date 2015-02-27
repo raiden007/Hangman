6836ccc971e243276e345dfa583a77df75531526
S <s>
2015-02-27 18:02:29 +0200

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -26,8 +26,9 @@ public class HangmanCanvas extends GCanvas {
 	public void displayWord(String word) {	
 		GLabel guessedWord = new GLabel (word);
 		guessedWord.setLocation(100, 450);
-		add (guessedWord);
 		remove(guessedWord);
+		add (guessedWord);
+
 	}
 
 /**
